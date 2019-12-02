6c555b5a4d58f78e66cf83a01ca4b400c820982d
Matthew Manley <MatthewManley@users.noreply.github.com>
2019-12-02 15:53:56 -0500

    wip, working encryption

--- encryption.py
+++ encryption.py
@@ -8,27 +8,40 @@ from Crypto.Util.Padding import pad, unpad
 key_bytes = 32
 
 
-# Encrypts a string using a key provided in hex. 
+# Encrypts a string using a key provided in hex.
 # Returns the IV and ciphertext in hex.
 def encryptAesCbc(cleartext: str, key_hex: str):
-    key_bytes = hex_to_bytes(key_hex)
+    # converts the hex key into bytes
+    key_bytes: bytes = hex_to_bytes(key_hex)
+
+    # Creates the new cipher object
     cipher = AES.new(key_bytes, AES.MODE_CBC)
-    encrypted_bytes = cipher.encrypt(pad(cleartext, AES.block_size))
+
+    # Encrypts the plaintext
+    encrypted_bytes = cipher.encrypt(pad(cleartext.encode('utf-8'), AES.block_size))
+
+    # Convert encrypted bytes to hex string
     encrypted_hex = bytes_to_hex(encrypted_bytes)
+
+    # Converts the generated IV to hex string
     iv_hex = bytes_to_hex(cipher.iv)
+
+    # Returns tuple with iv and encrypted hex
     return (iv_hex, encrypted_hex)
 
-# Dencrypts a string using a key and IV provided in hex. 
+
+# Dencrypts a string using a key and IV provided in hex.
 # Returns the cleartext.
 def decryptAesCbc(encrypted_hex: str, key_hex: str, iv_hex: str) -> str:
     encrypted_bytes = hex_to_bytes(encrypted_hex)
     key_bytes = hex_to_bytes(key_hex)
     iv_bytes = hex_to_bytes(iv_hex)
     cipher = AES.new(key_bytes, AES.MODE_CBC, iv_bytes)
-    return unpad(cipher.decrypt(encrypted_bytes, AES.block_size))
+    return unpad(cipher.decrypt(encrypted_bytes), AES.block_size)
+
 
 # Encrypts a string using SHA256
-# Returns the digest as 
+# Returns the digest as
 def encryptSha256(cleartext: str) -> str:
     cleartext_bytes = cleartext.encode('utf-8')
     sha = hashlib.sha256()
@@ -36,35 +49,43 @@ def encryptSha256(cleartext: str) -> str:
     digest_bytes = sha.digest()
     return bytes_to_hex(digest_bytes)
 
+
 # Generates length number of bytes and returns it as a hex string
 def generate_key(length: int):
     random_bytes = get_random_bytes(32)
     return bytes_to_hex(random_bytes)
 
+
 # Converts bytes to a hex string
-def bytes_to_hex(data: bytes, byte_order = 'big') -> str:
+def bytes_to_hex(data: bytes, byte_order='big') -> str:
     integer = int.from_bytes(data, byte_order)
-    return hex(integer)[2:] # Chop off the 0x at the start of the string
+    return hex(integer)[2:]  # Chop off the 0x at the start of the string
+
 
 # Converts a hex string to bytes
-def hex_to_bytes(data: str, byte_order = 'big') -> bytes:
+def hex_to_bytes(data: str, byte_order='big') -> bytes:
     return bytearray.fromhex(data)
+
 
 class InvertedIndex:
     # Dictionary mapping tokens to lists of values
     data = {}
 
-    def __init__(self, dataJson={}):
-        self.data = json.load(dataJson)
+    def __init__(self, dataJson: str = None):
+        if dataJson != None:
+            self.data = json.loads(dataJson)
 
-    def serialize(self):
+    def serialize(self) -> str:
         return json.dumps(self.data)
 
+    # token is encrypted word
+    # value is file name
     def addEntry(self, token, value):
         if (token in self.data):
             self.data[token].append(value)
         else:
             self.data[token] = [value]
 
-    def search(self, token):
+    # Returns a list of files that the word can be found in
+    def search(self, token) -> list:
         return self.data[token]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import file_io
-from encryption import generate_key, InvertedIndex, encryptSha256
+from encryption import generate_key, InvertedIndex, encryptSha256, encryptAesCbc
 
 key = 'key'
 func = 'func'
@@ -10,7 +10,8 @@ func = 'func'
 def keygen(args: list):
     expected = 1
     if len(args) != expected:
-        print(f'Invalid number of arugments for function keygen, got {len(args)}, expected {expected}')
+        print(
+            f'Invalid number of arugments for function keygen, got {len(args)}, expected {expected}')
         return
     path = args[0]
     key = generate_key(256 / 8)
@@ -20,26 +21,33 @@ def keygen(args: list):
 def encryption(args: list):
     expected = 4
     if len(args) != expected:
-        print(f'Invalid number of arugments for function keygen, got {len(args)}, expected {expected}')
+        print(
+            f'Invalid number of arugments for function encryption, got {len(args)}, expected {expected}')
         return
-    key_file = args[0] # Read the key from this file
-    index_file = args[1] # Write the index to this file 
-    plaintext_folder = args[2] # Read all plaintexts from this folder
-    ciphertext_folder = args[3] # Write all ciphertexts to this folder
+    key_file = args[0]  # Read the key from this file
+    index_file = args[1]  # Write the index to this file
+    plaintext_folder = args[2]  # Read all plaintexts from this folder
+    ciphertext_folder = args[3]  # Write all ciphertexts to this folder
 
     key_file_contents = file_io.read_file(key_file)
 
     # Create new inverted index
     inverted_index = InvertedIndex()
 
-    # Read in all files in plaintext folder
-    plaintext_file_names = list(filter(
-        lambda file_name: os.path.isfile(os.path.join(plaintext_folder, file_name)),
+    if not os.path.exists(ciphertext_folder):
+        os.makedirs(ciphertext_folder)
+
+    # Find the names of all files in the plaintext folder
+    plaintext_file_names = filter(
+        lambda file_name: os.path.isfile(
+            os.path.join(plaintext_folder, file_name)),
         os.listdir(plaintext_folder))
-    )
 
     for plaintext_file_name in plaintext_file_names:
-        plaintext = file_io.read_file(plaintext_file_name)
+        print(f'Processing file: {plaintext_file_name}')
+        full_plaintext_path = os.path.join(
+            plaintext_folder, plaintext_file_name)
+        plaintext = file_io.read_file(full_plaintext_path)
         words = plaintext.split(' ')
 
         # Add file to index
@@ -47,8 +55,14 @@ def encryption(args: list):
             inverted_index.addEntry(encryptSha256(word), plaintext_file_name)
 
         # Encrypt file and write to ciphertext folder
+        iv_hex, encrypted_hex = encryptAesCbc(plaintext, key_file_contents)
+        enc_file_content = f'{iv_hex}\n{encrypted_hex}'
+        full_cipher_path = os.path.join(
+            ciphertext_folder, plaintext_file_name.replace('f', 'c'))
+        file_io.write_file(full_cipher_path, enc_file_content)
 
     # Write inverted index file to disk
+    file_io.write_file(index_file, inverted_index.serialize())
 
 
 def token_generation(args: list):
@@ -56,6 +70,22 @@ def token_generation(args: list):
 
 
 def search(args: list):
+    expected = 4
+    if len(args) != expected:
+        print(
+            f'Invalid number of arugments for function search, got {len(args)}, expected {expected}')
+        return
+    index_file = args[0]  # Read the index from this file
+    token_file = args[1]  # Read the token from this file
+    ciphertext_folder = args[2]
+    key_file = args[3]
+
+    index_file_contents = file_io.read_file(index_file)
+    key_file_contents = file_io.read_file(key_file)
+
+    # Create new Inv
+    inverted_index = InvertedIndex()
+
     pass
 
 
@@ -79,7 +109,7 @@ def main(args: list):
         }
     ]
     try:
-        first_arg = args[0].lower()
+        first_arg = args[1].lower()
     except IndexError:
         print('First argument missing')
         return
@@ -93,7 +123,7 @@ def main(args: list):
         return
 
     # Call the function with the rest of the arugments
-    thing[func](args[1:])
+    thing[func](args[2:])
 
 
 if __name__ == "__main__":
